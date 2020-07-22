d6deceadc9d2603e5ed77d267413bc3557c092b6
Reggie Bigornia <reggietheroman@gmail.com>
2020-07-22 10:47:34 +0800

    Changed infrastructure/repository to infrastructure/repositories.

--- src/infrastructure/express/routes/plants.js
+++ src/infrastructure/express/routes/plants.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const ExpressAdaptor = require('../expressAdaptor');
-const plantRepository = require('../../repository/mongoose/plants');
+const plantRepository = require('../../repositories/mongoose/plants');
 const plantsController = require('../../../interfaces/controllers/plants');
 const plantsSerializer = require('../../../interfaces/serializers/plants');
 
